33b2c94a7d642d854d305fb10708a4a1f96d0d99
ferentinoskonstantinos <77170674+ferentinoskonstantinos@users.noreply.github.com>
2021-01-11 18:01:12 +0200

    GAM Analysis
    Examining pre-intervention price trends via generalized additive models

--- gam.R
+++ gam.R
@@ -1,3 +1,8 @@
+# This R script can be used to replicate the GAM analysis
+# for the paper "Climate Policy and Transition Risk in the Housing Market"
+# by Konstantinos Ferentinos & Alex Gibberd & Benjamin Guin.
+# The code was developed by Konstantinos Ferentinos.
+
 library(dplyr)
 library(plyr)
 library(readr)
@@ -11,9 +16,27 @@ library(mgcViz)
 library(scales)
 library(cowplot)
 
-## Examining Pre-intervention Price Trends via Generalized Additive Models ##
+# The DiD analysis is valid only under the assumption that the outcomes in treatment 
+# and control group follow the same time trend in the absence of the intervention.
+# Yet, the standard approach does not go beyond visual inspections 
+# and testing for differences in linear pre-intervention trends.
+# Assuming a linear form for the outcome trend, restricts one from fitting a wider range 
+# of possible shapes of the trend, that might be closer to its true functional form.
+# As the standard approach for evaluating this assumption seems restrictive, 
+# I present a novel assessment of the assumption, 
+# using the GAM with factor-smooth interactions framework, 
+# which formalizes the test for the statistical significance of the difference 
+# in pre-intervention non-linear price trends between the treatment and control group.
 
-data<-fread('D:\\psm_data_main.csv', header = T, 
+# In order to make the R code portable,
+# whenever I intend to import or save data in a CSV format
+# I define a variable with the name 'my_path' early in each R script 
+# that stores the path to each CSV file that is used in the code.
+# That way each user of the code can easily change the path at will,
+# thus improving its reproducibility.
+my_path<-'data\\'
+
+data<-fread(paste(my_path, 'psm_data_main.csv', sep='\\'), header = T, 
             data.table=FALSE)
 head(data)
 dim(data)
@@ -37,29 +60,25 @@ levels(data$EPC_LEVEL)<-c('Above E', 'Below E')
 data$EPC_LEVEL <- relevel(data$EPC_LEVEL, ref="Below E")
 
 
-# GAM with factor-smooth interactions
-fit1 <- gam(Price ~ EPC_LEVEL + s(Time, by = EPC_LEVEL, bs="cr"), data = data)
-summary(fit1)
-
-
 # GAM with ordered-factor-smooth interactions
 
-# We change EPC_LEVEL to an ordered factor
+# We treat EPC_LEVEL as an ordered factor
+# in order to fit a smooth function for treated properties, 
+# and then another smooth that models the difference between treated 
+# and non-treated properties.
 data$EPC_ordered <- as.ordered(data$EPC_LEVEL)
 contrasts(data$EPC_ordered) <- 'contr.treatment'
 
-fit2 <- gam(Price ~ EPC_ordered + s(Time, bs="cr") + s(Time, by=EPC_ordered, bs="cr"), data=data)
-summary(fit2)
-
-# This is the GAM that is reported in the paper.
-fit3 <- gam(Price ~ EPC_ordered + NUTS118NM + s(Time, bs="cr") + s(Time, by=EPC_ordered, bs="cr") +
+# We replicate Table 7 of the paper.
+fit <- gam(Price ~ EPC_ordered + NUTS118NM + s(Time, bs="cr") + s(Time, by=EPC_ordered, bs="cr") +
               Group1 + PROPERTY_TYPE + TOTAL_FLOOR_AREA + CONSTRUCTION_AGE_BAND + TENURE +
               NUMBER_HABITABLE_ROOMS_grouped, data=data)
-summary(fit3)
+summary(fit)
 
-b <- getViz(fit3)
+# We replicate Figure 5 of the paper.
+b <- getViz(fit)
 
-# Estimated smooth trend for properties with EPC rating below E 
+# Fitted price trend for properties with EPC rating below E 
 plot(sm(b, 1)) + l_fitLine(colour = "black", size = 0.8) +
   l_ciLine(level = 0.95, colour = "black", linetype = 2, size = 0.8) +
   scale_x_continuous(name="Date",
@@ -75,7 +94,7 @@ plot(sm(b, 1)) + l_fitLine(colour = "black", size = 0.8) +
         text = element_text(size=20),
         legend.text=element_text(size=20))
 
-# Estimated smooth difference trend between 'Below E' and 'Above E'
+# Estimated smooth for the difference between 'Below E' and 'Above E'
 plot(sm(b, 2)) + l_fitLine(colour = "black", size = 0.8) +
   l_ciLine(level = 0.95, colour = "black", linetype = 2, size = 0.8) +
   scale_x_continuous(name="Date",
